d9778686164a5ab49cd392d1aded093f0c1a8a9d
Mohamad-Aldebs1 <asustufa16mohamad@gmail.com>
2025-01-10 01:30:51 +0300

    dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,5 +27,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProductSeeder::class
         ]);
+        $this->call([
+            UserSeeder::class
+        ]);
     }
 }
